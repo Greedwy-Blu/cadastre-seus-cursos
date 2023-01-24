51bef6b93c73c8a6b1ba7b5ba9112419233e2fdb
Greedwy-Blu <greedwyblu@gmail.com>
2023-01-24 18:03:12 -0300

    refactor: configurando o app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from './infra/database/prisma/prisma.service';
 import { AppController } from './app.controller';
 import { userRepository } from './infra/database/repositories/user-repositories';
+import { studyRepository } from './infra/database/repositories/study-repositories';
+import { PrismaStudyRepository } from './infra/database/repositories/prisma/prisma-study-repositories';
 @Module({
   imports: [],
   controllers: [AppController],
@@ -11,6 +13,10 @@ import { userRepository } from './infra/database/repositories/user-repositories'
     {
       provide: userRepository,
       useClass: PrismaUserRepository,
+    },
+    {
+      provide: studyRepository,
+      useClass: PrismaStudyRepository,
     },
   ],
 })
