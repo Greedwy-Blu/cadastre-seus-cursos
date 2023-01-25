5a0cd8b36e538f879bb46fe74f56016de4dd5442
Greedwy-Blu <greedwyblu@gmail.com>
2023-01-25 17:21:02 -0300

    feat: configuração do auth

--- src/infra/http/auth/auth.service.ts
+++ src/infra/http/auth/auth.service.ts
@@ -1,4 +1,16 @@
+import { userService } from './../../user/user.service';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: userService) {}
+
+  async validateUser(email: string, pass: string): Promise<any> {
+    const user = await this.usersService.userFind(email);
+    if (user && user.password === pass) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+}
