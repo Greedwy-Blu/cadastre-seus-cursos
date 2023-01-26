c2876bff4c5cfbafbbfaa3ce850c758411cc1f22
Greedwy-Blu <greedwyblu@gmail.com>
2023-01-26 18:41:27 -0300

    refactor: mudanças no auth.service e jwt strategy

--- src/infra/http/auth/auth.service.ts
+++ src/infra/http/auth/auth.service.ts
@@ -19,7 +19,7 @@ export class AuthService {
   }
 
   async login(user: any) {
-    const payload = { name: user.name, sub: user.id };
+    const payload = { username: user.name, sub: user.id };
     return {
       access_token: this.jwtService.sign(payload),
     };
