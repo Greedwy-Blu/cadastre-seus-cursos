bdb36fde1342aa588c258738e83827cfc1bf23a9
Greedwy-Blu <greedwyblu@gmail.com>
2023-01-26 13:33:12 -0300

    refactor: importação do AuthModule

--- src/infra/user/user.module.ts
+++ src/infra/user/user.module.ts
@@ -1,9 +1,12 @@
+import { AuthModule } from './../http/auth/auth.module';
 import { createUserBody } from './../dtos/create-user-body';
 import { PrismaService } from './../database/prisma/prisma.service';
 import { Module } from '@nestjs/common';
 import { userService } from './user.service';
+import { forwardRef } from '@nestjs/common/utils';
 
 @Module({
+  imports: [forwardRef(() => AuthModule)],
   providers: [userService, PrismaService, createUserBody],
   exports: [userService],
 })
