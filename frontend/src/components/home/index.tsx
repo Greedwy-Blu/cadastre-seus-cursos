a7578e8f8e97eb2d40964d4f2fbdba1c08884852
Greedwy-Blu <greedwyblu@gmail.com>
2023-02-19 22:41:41 -0300

    feat: atualizado homePage

--- frontend/src/components/home/index.tsx
+++ frontend/src/components/home/index.tsx
@@ -1,9 +1,11 @@
-import imgHome from '../../assets/student-svgrepo-com.svg'
+import imgHome from '../../assets/undraw_knowledge_re_5v9l.svg'
 export default function HomePage(){
 return(
-  <section className='flex justify-items-center w-[900px] h-[500px]'>
-    <div className='flex items-center '>
-      <p className='ml-10 h-32 w-72'>
+  <section className='grid justify-items-center w-[900px] h-[500px]'>
+    <div className='flex items-center translate-x-28'>
+
+    <div className=''>
+      <p className='h-48 w-[500px]'>
         <a className="text-center text-3xl ">
 
           Organizes as suas grades, o controle o seus estudos, melhorando a sua eficaz e
@@ -12,8 +14,10 @@ return(
       </p>
     </div>
 
-    <div>
-      <img src="" alt="" />
+    <div className=''>
+      <img src={imgHome} alt="" className='h-[500px] w-[800px]' />
+    </div>
+
     </div>
 
   </section>
