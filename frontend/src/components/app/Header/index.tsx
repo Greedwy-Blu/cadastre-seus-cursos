f1eb3545f3e797b68deccdbac5718111092efafe
Greedwy-Blu <greedwyblu@gmail.com>
2023-02-20 18:49:24 -0300

    feat: adicionado novas mudanças no layout do app

--- frontend/src/components/app/Header/index.tsx
+++ frontend/src/components/app/Header/index.tsx
@@ -7,16 +7,26 @@ import Profile from "../profile";
 
 export function HeaderApp(){
   return(
-    <section>
-      <div className="flex justify-center w-full h-12 bg-orange-200">
-      <ul className="flex space-x-1 m-5">
+    <section className="">
+      <div className="flex justify-center  w-full h-12 bg-orange-200">
+        <div className="flex ">
+        <ul className="flex  items-center space-x-1  font-bold">
         <Link to="/app/profile">
           <li><a className="bg-red-200 h-10 w-11 p-2  hover:bg-red-500" >perfil</a></li>
           </Link>
-          <Link to="/app/profile" >
-          <li><a className="bg-red-200 h-10 w-11 p-2  hover:bg-red-500" >materias</a></li>
+          <Link to="/app/study" >
+          <li><a className="bg-red-200 h-10 w-11 p-2  hover:bg-red-500" >estudos</a></li>
           </Link>
         </ul>
+        </div>
+
+        <div className="flex  translate-x-[500px] ">
+          <div className="flex items-center">
+          <p className=" i font-bold "><a>Logout</a></p>
+          </div>
+        </div>
+
+
       </div>
     </section>
   )
--- frontend/src/components/app/profile/index.tsx
+++ frontend/src/components/app/profile/index.tsx
@@ -1,20 +1,65 @@
 import { Link } from "react-router-dom";
 
+import imgIcon from '../../../assets/undraw_reading_re_29f8.svg'
+
 export default function Profile(){
   return(
     <section>
-        <div className="flex justify-center w-full h-16 bg-orange-200">
-        <ul className="flex space-x-1 m-5">
+        <div className="flex justify-center  w-full h-12 bg-orange-200">
+        <div className="flex ">
+        <ul className="flex  items-center space-x-1  font-bold">
         <Link to="/app/profile">
-          <li><a className="bg-red-300 h-10 w-11 p-2 hover:bg-red-500" >perfil</a></li>
+          <li><a className="bg-red-200 h-10 w-11 p-2  hover:bg-red-500" >perfil</a></li>
           </Link>
           <Link to="/app/study" >
-          <li><a className="bg-red-300 h-10 w-11 p-2  hover:bg-red-500" >materias</a></li>
+          <li><a className="bg-red-200 h-10 w-11 p-2  hover:bg-red-500" >estudos</a></li>
           </Link>
         </ul>
+        </div>
+
+        <div className="flex  translate-x-[500px] ">
+          <div className="flex items-center">
+          <p className=" i font-bold "><a>Logout</a></p>
+          </div>
+        </div>
+
+
       </div>
-      <div>
-        <a>aaabbb</a>
+
+      <div className="  w-[500px] h-[600px] mt-10 ml-[450px]">
+
+        <div className="flex justify-center">
+         <img src={imgIcon} alt="" className='h-32 w-32' />
+        </div>
+        <div>
+
+        <p className="flex font-bold  m-4 space-x-4">
+
+
+        <a>nome:</a>
+        <a>teste</a>
+
+        </p>
+
+        <p className="flex font-bold m-4 space-x-4">
+
+
+        <a>email:</a>
+        <a>teste</a>
+
+        </p>
+
+        <p className="flex font-bold  m-4 space-x-4">
+
+
+        <a>idade:</a>
+        <a>teste</a>
+
+        </p>
+
+        </div>
+
+
       </div>
     </section>
   )
