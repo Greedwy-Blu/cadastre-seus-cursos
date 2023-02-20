309efa92023b6939e1e0393ff5c9998f0184201b
Greedwy-Blu <greedwyblu@gmail.com>
2023-02-19 22:43:57 -0300

    feat: atualização no header

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -3,7 +3,7 @@ export default function Header(){
     <header>
       <div className="flex justify-center items-center bg-orange-500 w-full h-16">
 
-        <a className="text-lg font-bold">NotionGrade</a>
+        <a className="text-lg font-bold">organize suas aulas</a>
 
       </div>
     </header>
