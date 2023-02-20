f07c718804f21561c32f8acdccf330eda360f945
Greedwy-Blu <greedwyblu@gmail.com>
2023-02-20 00:11:25 -0300

    feat: atualização

--- frontend/src/components/Error.tsx
+++ frontend/src/components/Error.tsx
@@ -1,8 +1,14 @@
+import imgError from '../assets/error.png'
 export default function Error(){
 
   return(
-    <section className="flex justify-center">
-      <p className="text-5xl">ERRO: PAGÍNA  NÃO ENCONTRADA</p>
+    <section className="">
+      <div className="flex justify-center mt-52">
+      <p className="text-5xl">ERROR 404 : PAGÍNA  NÃO ENCONTRADA</p>
+      </div>
+      <div className='flex justify-center mt-10'>
+      <img src={imgError} alt="" className='h-24 w-34' />
+      </div>
     </section>
   )
 
--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,7 +1,12 @@
+
+import imgIcon from '../../assets/undraw_reading_re_29f8.svg'
 export default function Header(){
   return(
     <header>
       <div className="flex justify-center items-center bg-orange-500 w-full h-16">
+
+
+      <img src={imgIcon} alt="" className='h-12 w-12' />
 
         <a className="text-lg font-bold">organize suas aulas</a>
 
