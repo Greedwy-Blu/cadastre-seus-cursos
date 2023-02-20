e75bdc454fb411140233314c20b7f8af63ad0ecc
Greedwy-Blu <greedwyblu@gmail.com>
2023-02-19 23:58:10 -0300

    feat: adicionado pagina de erro

---
+++ frontend/src/components/Error.tsx
@@ -0,0 +1,9 @@
+export default function Error(){
+
+  return(
+    <section className="flex justify-center">
+      <p className="text-5xl">ERRO: PAGÍNA  NÃO ENCONTRADA</p>
+    </section>
+  )
+
+}
--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './components/home';
 import Login from './components/login';
 import Register from './components/register';
+import Error from './components/Error';
 
 
 
@@ -14,6 +15,7 @@ export default function  Routers(){
                     <Route path="/" element={<HomePage/>}/>
                     <Route path="Home/login" element={<Login/>}/>
                     <Route path="Home/register" element={<Register/>}/>
+                    <Route path='*' element={<Error/>} />
 
                </Routes>
 
